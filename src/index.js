0eb4613cc8ca923504b38935b6692a5a8d8f8b14
Lauren <lauren@tomasello.me>
2017-09-10 20:46:35 +0100

    Use real results for donut chart

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import * as d3 from 'd3'
-import donut   from './donut.js'
-import bar     from './bar.js'
-import data    from '../data/anonymised.csv'
+import * as d3   from 'd3'
+import donut     from './donut.js'
+import bar       from './bar.js'
+import data      from '../data/anonymised.csv'
+
+console.log(data);
 
 document.addEventListener('DOMContentLoaded', _ => {
 	const labels = ['Unknown', 'Male', 'Female', 'Non-binary']
 
 	d3.select("#results").selectAll("*").remove()
 	
-	donut(d3.select('#results'), data)
+	donut(d3.select('#results'), labels.map(
+		label => ({
+			label,
+			value: data
+				.filter(entry => label === decode_pronoun_to_label(entry.Pronoun))
+				.length
+		})
+	))
 	bar(d3.select('#results'), data)
 })
+
+function decode_pronoun_to_label(pronoun) {
+	return ({
+		'he': 'Male',
+		'she': 'Female',
+		'they': 'Non-binary',
+	}[pronoun.toLowerCase()]) || 'Unknown'
+}
