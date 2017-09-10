3b04183b4c7a6ec5fa045abc475fbaaa8deb3ac3
Lauren <lauren@tomasello.me>
2017-09-10 23:29:22 +0100

    Add final two charts

--- src/index.js
+++ src/index.js
@@ -30,17 +30,20 @@ document.addEventListener('DOMContentLoaded', _ => {
 	))
 
 	// Fig 3. Gender composition of AES conference across each year, where the number on the right is the population size for each year.
-	bar($results, data_grouped_by_selector('Topic'))
+	bar($results, grouped_by_selector(data, 'Topic'))
 	// Fig. 4 Gender composition across conference topics, where the number on the right is the population size for each topic.
-	bar($results, data_grouped_by_selector('Year'))
+	bar($results, grouped_by_selector(data, 'Year'))
 	// Fig 5. Gender composition across the different presentation types, where the asterisk indicates an ‘invited’ paper and number on the right is the population size for each presentation type.
-	bar($results, data_grouped_by_selector('Grouped Type'))
-
+	bar($results, grouped_by_selector(data, 'Grouped Type'))
 	// Fig. 6 Gender composition across relative author positions in presentations where the number on the right is the population size for each author position.
+	bar($results, grouped_by_selector(
+		data.filter(d => d['Inc. for author pos?'] === 'TRUE'), 'Position (relative)'))
 	// Fig. 7 Gender composition of single authors vs multi-authored presentations across all conferences, where the number on the right is the population size for each authorship type.
+	bar($results, grouped_by_selector(
+		data.filter(d => d['Inc. for sole/coauthor?'] === 'TRUE'), 'Single/multi author'))
 })
 
-function data_grouped_by_selector(selector) {
+function grouped_by_selector(data, selector) {
 	return uniq(data.map(d => d[selector])).map(
 		label => ({
 			label,
