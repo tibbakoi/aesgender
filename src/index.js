786068f3906f1d17e2e21c0296b58d98c2202506
Lauren <lauren@tomasello.me>
2017-09-17 20:31:08 +0100

    Expand comments

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import {uniq, zipObject}
 
 import unprocessed_data from '../data/anonymised.csv'
 
-// Add normalized gender field
+// Add normalized gender field to data from CSV file
 let data = unprocessed_data.map(d => ({
 	'Gender': decode_pronoun_to_label(d.Pronoun),
 	...d,
 }))
 
+// The groups on the charts
 const genders = ['Unknown', 'Male', 'Female', 'Non-binary']
 
+// Don't add charts until page has loaded
 document.addEventListener('DOMContentLoaded', _ => {
 
 	const $results = d3.select("#results")
@@ -42,6 +44,7 @@ document.addEventListener('DOMContentLoaded', _ => {
 			data.filter(d => d['Inc. for sole/coauthor?'] === 'TRUE'), 'Single/multi author')),
 	]
 
+	// Responsive design
 	window.addEventListener('resize', e => {
 		// Don't use .getElementsById because HTMLCollection doesn't implement
 		// iterator methods
@@ -58,6 +61,8 @@ document.addEventListener('DOMContentLoaded', _ => {
 	})
 })
 
+// Returns a transformed version of the data grouped by a specific selector field
+// i.e. all entries where the given field is the same will be grouped together
 function grouped_by_selector(data, selector) {
 	return uniq(data.map(d => d[selector])).map(
 		label => ({
@@ -70,6 +75,8 @@ function grouped_by_selector(data, selector) {
 	)
 }
 
+// Converts from the pronouns stored in the CSV file into a 'Gender' field
+// for grouping in the charts
 function decode_pronoun_to_label(pronoun) {
 	return ({
 		he:   'Male',
