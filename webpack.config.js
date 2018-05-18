af03219e8f678b2cf8e8c825daa5492d53510ded
Lauren <lauren@tomasello.me>
2018-05-18 17:18:45 +0100

    Update webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path    = require('path')
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
 	entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -10,8 +11,8 @@ module.exports = {
 	},
 	// Don't bundle these into the output
 	externals: {
-		//lodash: '_',
-		//d3: 'd3',
+		lodash: '_',
+		d3: 'd3',
 	},
 	// Load any js files through babel for polyfills etc
 	module: {
@@ -33,13 +34,18 @@ module.exports = {
 	},
 	// Hot module reloading for CSS etc
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
+		//new webpack.HotModuleReplacementPlugin(),
 		new CopyWebpackPlugin([{from: 'src/assets/**/*', to: 'assets/[name].[ext]'}]),
+		new UglifyJSPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
 	],
+	/*
 	devtool: 'inline-source-map',
 	devServer: {
 		contentBase: path.join(__dirname, "dist"),
 		hot: true,              // Enable HMR
 		watchContentBase: true, // Needed to auto update when index.html changes
-	},
+	},*/
 }
